5f66ec593228256f2ab94742629ab25c31069ad0
bugaj55113 <bbugi2016@gmail.com>
2023-12-16 16:38:09 +0100

    Reports Screen added

---
+++ app/src/main/java/com/example/locationguardpro/ReportsScreenActivity.kt
@@ -0,0 +1,11 @@
+package com.example.locationguardpro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ReportsScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reports_screen)
+    }
+}
