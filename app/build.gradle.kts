b228b08ff9e837405708f750faf6b7b527286586
bugaj55113 <bbugi2016@gmail.com>
2023-12-17 16:30:15 +0100

    Added ViewPager to handle the calendar and the chart on one screen.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ android {
     }
 }
 
+
 dependencies {
 
     implementation("androidx.core:core-ktx:1.9.0")
@@ -48,6 +49,8 @@ dependencies {
     implementation ("com.google.android.gms:play-services-maps:17.0.1")
     implementation ("com.google.android.gms:play-services-location:17.1.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation ("androidx.viewpager:viewpager:1.0.0")
+    implementation("com.tbuonomo:dotsindicator:5.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
