6b95517deac1277f0a463e89852912832f7b5345
bugaj55113 <bbugi2016@gmail.com>
2024-01-27 18:46:35 +0100

    Callendar corrected.

--- app/src/main/java/com/example/locationguardpro/CalendarFragment.kt
+++ app/src/main/java/com/example/locationguardpro/CalendarFragment.kt
@@ -1,12 +1,19 @@
+import android.content.Context
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import com.example.locationguardpro.MyApplication
 import com.example.locationguardpro.R
 import com.github.sundeepk.compactcalendarview.CompactCalendarView
 import com.github.sundeepk.compactcalendarview.domain.Event
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
 import java.util.*
 
 class CalendarFragment : Fragment() {
@@ -17,19 +24,56 @@ class CalendarFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_calendar, container, false)
 
+        val myApplication = requireActivity().application as MyApplication
+        val appDatabase = myApplication.appDatabase
+        val workHoursDao = appDatabase.workHoursDao()
+        val sharedPreferences =
+            requireActivity().getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+        val userId = sharedPreferences.getLong("USER_ID", -1)
+
         val compactCalendarView = view.findViewById<CompactCalendarView>(R.id.calendarView)
         compactCalendarView.setFirstDayOfWeek(Calendar.MONDAY)
 
-        // Przykładowe dane o przepracowanych godzinach dla różnych dni
-        val workHoursMap = hashMapOf(
-            getDateInMillis(2024, Calendar.JANUARY, 10) to 2.5,  // 10 stycznia 2024 - przykładowe godziny
-            getDateInMillis(2024, Calendar.JANUARY, 10, 1, 30) to 6.0   // 10 stycznia 2024 - przykładowe godziny
-        )
+        // Uruchom korutynę w zakresie komponentu życia fragmentu
+        viewLifecycleOwner.lifecycleScope.launch(Dispatchers.Main) {
+            // Pobierz dane z bazy danych w kontekście korutyny
+            val workHoursList = workHoursDao.getWorkHoursByUserId(userId)
 
-        for ((timestamp, workHours) in workHoursMap) {
-            val color = getColorForWorkHours(workHours)
-            val event = Event(color, timestamp)
-            compactCalendarView.addEvent(event)
+            // Usunięcie duplikatów z listy dat
+            val uniqueDatesList = workHoursList.distinct()
+            val workHoursMap = HashMap<Long, Double>()
+            val uniqueDateStrings = HashSet<String>()
+            val uniqueDatesWithoutDuplicates = ArrayList<Date>()
+
+            for (workHours in uniqueDatesList) {
+                val dateString = workHours.date
+
+                // Parsowanie daty
+                val date = SimpleDateFormat("dd.MM.yyyy").parse(dateString)
+
+                // Dodawanie tylko unikalnych dat do listy
+                if (uniqueDateStrings.add(dateString)) {
+                    uniqueDatesWithoutDuplicates.add(date)
+                }
+            }
+
+            for (uniqueDate in uniqueDatesWithoutDuplicates) {
+
+                val calendar = Calendar.getInstance()
+                    calendar.time = uniqueDate
+                val dateInMillis = calendar.timeInMillis
+                val outputFormat = SimpleDateFormat("dd.MM.yyyy")
+                val dateInString = outputFormat.format(uniqueDate)
+                val myWorkHoursForDate = workHoursDao.getTotalHoursForDate(userId, dateInString)
+                Log.d("datetest", myWorkHoursForDate.toString())
+                workHoursMap[dateInMillis] = myWorkHoursForDate
+            }
+
+            for ((timestamp, workHours) in workHoursMap) {
+                val color = getColorForWorkHours(workHours)
+                val event = Event(color, timestamp)
+                compactCalendarView.addEvent(event)
+            }
         }
 
         compactCalendarView.setListener(object : CompactCalendarView.CompactCalendarViewListener {
@@ -44,12 +88,6 @@ class CalendarFragment : Fragment() {
         })
 
         return view
-    }
-
-    private fun getDateInMillis(year: Int, month: Int, day: Int, hour: Int = 0, minute: Int = 0, second: Int = 0): Long {
-        val calendar = Calendar.getInstance()
-        calendar.set(year, month, day, hour, minute, second)
-        return calendar.timeInMillis
     }
 
     private fun getColorForWorkHours(workHours: Double): Int {
